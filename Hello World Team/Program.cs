d7b6d20ca0d4476f2850a4cf4371c356aca544b9
Paul Lesieur <plesieur@jpkeefehs.org>
2021-03-04 07:39:13 -0500

    Add project files.

---
+++ Hello World Team/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World_Team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am on Team ColorGoesHere");
+            Console.WriteLine("My name is NameGoesHere");
+        }
+    }
+}
